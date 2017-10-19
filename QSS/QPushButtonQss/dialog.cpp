59a8a33ddaf3a04bcb4484cfbd19107537207569
lesliefish <yuleics@outlook.com>
2017-10-19 14:20:23 +0800

    update
    update

--- QSS/QPushButtonQss/dialog.cpp
+++ QSS/QPushButtonQss/dialog.cpp
@@ -1,11 +1,20 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QFile>
+#include <Qdir>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    //加载Style
+    QString path = QDir::currentPath() + "/../QPushButtonQss/button.qss";
+    QFile qssFile(path);
+    qssFile.open(QFile::ReadOnly);
+    qApp->setStyleSheet(qssFile.readAll());
+    qssFile.close();
 }
 
 Dialog::~Dialog()
