861260760aca2c5ef81746f19c37d140a3e5f3cc
leiyu <yuleics@outlook.com>
2017-10-31 22:28:51 +0800

    update
    update

--- Core/字符串设置固定宽度，前位补0/字符串设置固定宽度，前位补0.cc
+++ Core/字符串设置固定宽度，前位补0/字符串设置固定宽度，前位补0.cc
@@ -1,3 +1,5 @@
-    QString str= QString::number(12);
-    //10进制数 12  4位  前位补0  strNew = "0012"
+    QString str= QString::number(17);
+    //10进制数 17  4位  前位补0  strNew = "0017"
     QString strNew = QString("%1").arg(str.toInt(), 4, 10, QLatin1Char('0'));
+	
+	QString strNew16 = QString("%1").arg(str.toInt(), 4, 16, QLatin1Char('0'));
