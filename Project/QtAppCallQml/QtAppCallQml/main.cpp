1100f16f411cfc968ea06e73389288bf1b196bf4
leiyu <ylesliefish@gmail.com>
2019-11-04 23:19:39 +0800

    tab replaced by space

--- Project/QtAppCallQml/QtAppCallQml/main.cpp
+++ Project/QtAppCallQml/QtAppCallQml/main.cpp
@@ -6,31 +6,32 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	QQuickWidget w;
-	w.setSource(QUrl(QStringLiteral("qrc:/QtAppCallQml/Resources/main.qml")));
-	w.setResizeMode(QQuickWidget::SizeRootObjectToView);
-	w.show();
+    QApplication a(argc, argv);
+    QQuickWidget w;
+    w.setSource(QUrl(QStringLiteral("qrc:/QtAppCallQml/Resources/main.qml")));
+    w.setResizeMode(QQuickWidget::SizeRootObjectToView);
+    w.show();
 
-	QPushButton btn(&w);
-	btn.setText("Change Page 1 Color to lightblue");
-	btn.show();
-	auto swipeView = w.rootObject()->findChild<QObject*>("swipeViewObj");
-	if (swipeView != nullptr)
-	{
-		auto page1 = swipeView->findChild<QObject*>("page1Obj"); // 点击按钮设置page1背景色lightblue
-		if (page1 != nullptr)
-		{
-			QObject::connect(&btn, &QPushButton::clicked, [&] {
-				QMetaObject::invokeMethod(page1, "setColor", Q_ARG(QVariant, "lightblue"));
-				// 或者 			page1->setProperty("color", "lightblue");
-			});
-		}
+    QPushButton btn(&w);
+    btn.setText("Change Page 1 Color to lightblue");
+    btn.show();
+    auto swipeView = w.rootObject()->findChild<QObject*>("swipeViewObj");
+    if (swipeView != nullptr)
+    {
+        auto page1 = swipeView->findChild<QObject*>("page1Obj"); // 点击按钮设置page1背景色lightblue
+        if (page1 != nullptr)
+        {
+            QObject::connect(&btn, &QPushButton::clicked, [&]
+            {
+                QMetaObject::invokeMethod(page1, "setColor", Q_ARG(QVariant, "lightblue"));
+                // 或者 			page1->setProperty("color", "lightblue");
+            });
+        }
 
-		// qml的swipeView页面切换时，最大化显示
-		QObject::connect(swipeView, SIGNAL(currentIndexChanged()), &w, SLOT(showMaximized()));
-	}
+        // qml的swipeView页面切换时，最大化显示
+        QObject::connect(swipeView, SIGNAL(currentIndexChanged()), &w, SLOT(showMaximized()));
+    }
 
 
-	return a.exec();
+    return a.exec();
 }
