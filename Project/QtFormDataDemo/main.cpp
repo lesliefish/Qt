3b7ce9ce5cf16a1acc90e6fb8558437df5af400f
leiyu <yuleics@outlook.com>
2020-01-06 22:05:47 +0800

    Update main.cpp

--- Project/QtFormDataDemo/main.cpp
+++ Project/QtFormDataDemo/main.cpp
@@ -10,6 +10,7 @@ bool requestUploadImages(const QStringList& vsFileList, QString& strResp)
 	QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
 	for (const auto& filePathIter : vsFileList)
 	{
+		// 多个文件分成多个表单
 		QHttpPart imagePart;
 		QFileInfo filInfo(filePathIter);
 		QFile file(filePathIter);
@@ -18,21 +19,25 @@ bool requestUploadImages(const QStringList& vsFileList, QString& strResp)
 			continue;
 		}
 
-		imagePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant(QString("form-data; name=\"params\"; filename=\"%1\"").arg(filInfo.fileName())));
-		imagePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("image/png"));
+		//这个根据自己的协议添加对应数据
+		imagePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+			QVariant(QString("form-data; name=\"uploadfile\"; filename=\"%1\"").arg(filInfo.fileName()))); 
+		// 类型 根据自己需要的协议进行定义
+		imagePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("image/png")); 
 
 		const auto& data = file.readAll();
-		imagePart.setBody(data);
+		imagePart.setBody(data);// 表单body部分填入文件内容
+		// 数据长度不加上可能导致失败
 		imagePart.setHeader(QNetworkRequest::ContentLengthHeader, QVariant(data.size()));
 		multiPart->append(imagePart);
 	}
 
-	QUrl url("http://www.csu.edu.cn");
+	QUrl url("http://www.csu.edu.cn"); // 测试url 要换成自己实际使用的URI
 	QNetworkRequest request(url);
 	QNetworkAccessManager manager;
-	//QNetworkReply *reply = manager.post(request, multiPart);
-	QNetworkReply *reply = manager.get(request);
-	multiPart->setParent(reply); // delete the multiPart with the reply
+	QNetworkReply *reply = manager.post(request, multiPart);
+
+	multiPart->setParent(reply); // 通过reply删除multiPart对象
 
 	QObject::connect(reply, &QNetworkReply::finished, [&reply, &strResp]
 	{
@@ -65,9 +70,7 @@ int main(int argc, char *argv[])
 	QCoreApplication a(argc, argv);
 
 	QString resp;
-
 	requestUploadImages(QStringList(), resp);
-
 	qDebug() << resp;
 
 	return a.exec();
